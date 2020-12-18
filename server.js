7808d89926a47c420276c52d9cd1d63887a79096
Fabian Oraze <oraze.fabian@icloud.com>
2020-12-18 11:57:05 +0100

    changed port

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.static(__dirname + '/dist/client'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/client/index.html'));
 })
-app.listen(process.env.PORT || 8080, () => {
+
+app.listen(process.env.PORT || 5000, () => {
     console.log("Server listening...");
 })
