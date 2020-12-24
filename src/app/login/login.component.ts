83364bdd8c74f63623bf5da3d20533a881970151
elias <elsandner@edu.aau.at>
2020-12-24 11:32:18 +0100

    added cookie

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthServiceService } from '../auth-service.service';
 import {Router} from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-login',
@@ -10,11 +11,16 @@ import {Router} from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   formGroup: FormGroup;
+  
 
   constructor(
     private authService: AuthServiceService, 
-    public router: Router) 
-    {   }
+    public router: Router,
+    private cookieService: CookieService
+    ) 
+    {   
+           
+    }
 
   ngOnInit(): void {
     this.initForm();
@@ -30,16 +36,19 @@ export class LoginComponent implements OnInit {
   }
   
   loginProcess(){
-    console.log("try to login!!!!!!!!!!!!")
     if(this.formGroup.valid){
       this.authService.login(this.formGroup.value).subscribe(result=>{
-        console.log(result)
+        console.log("Login succeeded")
+        
 
+        //Cookies: https://www.npmjs.com/package/ngx-cookie-service
+        this.cookieService.set( 'token', result.body.token   ); 
+        this.cookieService.set( 'refreshToken', result.body.refreshToken ); 
         this.router.navigate(['saldo']);
 
       }), (error)=> {
           console.log(error)
-          alert("Wrong Password");
+          alert("Login failed");
       }
       }
     }
--- src/app/saldo/saldo.component.ts
+++ src/app/saldo/saldo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 
 
@@ -11,7 +12,7 @@ export class SaldoComponent implements OnInit {
 
   view: boolean = true; //used with *ngIf="view" in html to hide elements when entering a new spending 
 
-  constructor() { }
+  constructor(private cookieService: CookieService) { }
 
   ngOnInit(): void {
   }
@@ -19,6 +20,9 @@ export class SaldoComponent implements OnInit {
   addSpending(){
     console.log("add spending!")
     this.view=false;
+    console.log(this.cookieService.get("token"));
+    console.log(this.cookieService.get("refreshToken"));
+
   }
 
 }
